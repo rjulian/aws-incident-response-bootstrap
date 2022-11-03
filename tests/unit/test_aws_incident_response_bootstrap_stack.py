76cdcd78173faaaa2ba69ad6e9eed23e7b8589e0
Richard Julian <richard@rjulian.net>
2022-11-03 12:57:07 -0700

    Add in further tests for sns and eventbridge

--- tests/unit/test_aws_incident_response_bootstrap_stack.py
+++ tests/unit/test_aws_incident_response_bootstrap_stack.py
@@ -5,11 +5,33 @@ from aws_incident_response_bootstrap.aws_incident_response_bootstrap_stack impor
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in aws_incident_response_bootstrap/aws_incident_response_bootstrap_stack.py
-def test_sqs_queue_created():
+def test_guardduty_created():
     app = core.App()
     stack = AwsIncidentResponseBootstrapStack(app, "aws-incident-response-bootstrap")
     template = assertions.Template.from_stack(stack)
 
     template.has_resource_properties("AWS::GuardDuty::Detector", {
         "Enable": True
+    })
+
+def test_sns_created():
+    app = core.App()
+    stack = AwsIncidentResponseBootstrapStack(app, "aws-incident-response-bootstrap")
+    template = assertions.Template.from_stack(stack)
+
+    template.has_resource_properties("AWS::SNS::Topic", {
+        "DisplayName": "GuardDuty Alerting Topic"
+    })
+
+    template.has_resource_properties("AWS::SNS::Subscription", {
+        "Protocol": "email"
+    })
+
+def test_eventbridge_rule_and_target_created():
+    app = core.App()
+    stack = AwsIncidentResponseBootstrapStack(app, "aws-incident-response-bootstrap")
+    template = assertions.Template.from_stack(stack)
+
+    template.has_resource_properties("AWS::Events::Rule", {
+        "State": "ENABLED",
     })
